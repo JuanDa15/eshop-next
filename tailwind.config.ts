890bd7cbb6f0f97c7ee207cb6eeda2658c35791f
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2025-02-22 23:54:58 -0500

    setup tailwind variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "primary-background": "var(--background-color)",
+        "secondary-background": "var(--secondary-background-color)",
+        "primary-text": "var(--text-color)",
+        "secondary-text": "var(--secondary-text-color)",
+        "primary": "var(--primary-color)",
+        "secondary": "var(--secondary-color)",
+        "accent": "var(--accent-color)",
+        "boder-color": "var(--border-color)",
       },
     },
   },
